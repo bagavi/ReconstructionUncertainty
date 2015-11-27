829a268e31b3ccac4d92c7b3ca17ef5ce9d5097b
bagavi <vivekee047@gmail.com>
2015-11-26 21:02:50 -0800

    code

---
+++ CommonFunctions.py
@@ -0,0 +1,167 @@
+import random
+import numpy, csv, os
+from collections import OrderedDict
+
+def CdfFromPdf( Pdf ):
+    Cdf = [ Pdf[0] ]
+    for index, value in enumerate( Pdf[1:] ): 
+        Cdf.append( Cdf[index] + value )
+    return( Cdf )
+
+def SampleDistributionFromCdf( Cdf, Symbols):
+    TempRandom = random.random()
+    for index, value in enumerate( Cdf ):
+        if( TempRandom <= value):
+            return( Symbols[ index ])
+
+
+def SampleDistributionFromPdf( Pdf, Symbols):
+    Cdf = CdfFromPdf( tuple( Pdf ) )
+    return( SampleDistributionFromCdf(Cdf, Symbols))
+
+def MatrixFromDict( Dict ):
+    TMatrix = []
+    for i in Dict:
+        TMatrix.append( list( Dict[i].values() ) )
+    return( numpy.array( TMatrix ) )
+
+def InverseMatrix( Matrix ):
+    InvMatrix = numpy.linalg.inv( Matrix )
+    return( InvMatrix )
+                   
+def MultiplyVectorsComponenetWise( Vector1, Vector2): 
+    return( numpy.multiply( Vector1, Vector2))
+
+def PointWiseListDifference( List1, List2):
+    Temp = []
+    for i in range( len( List1) ):
+        if( List1[i] != List2[i] ):
+            Temp.append( 1 )
+        else:
+            Temp.append( 0 )
+    return( Temp )
+
+def CheckProbabilitiesSumtoOne( Array ):
+    for i in Array:
+        if( abs( sum(i) -  1 ) > .01 ):
+            print( "Probabilites not summing to one!")
+            
+def WriteArrayinFile( Array, Filename ):
+    with open( Filename, 'w', newline = '') as f:                                    
+            writer = csv.writer(f)                                                       
+            writer.writerows(Array)
+            
+def ReWriteArrayinFile( Array, Filename ):
+    try:
+        PreviousData = FiletoArray(Filename)
+        CombinedData = Array[0] + PreviousData + Array[1:]
+    except:
+        CombinedData = Array #Keeping the same heading
+    
+    WriteArrayinFile(CombinedData, Filename)
+    
+def FiletoArray( Filename = 'Results_posix.csv' , Int=False):
+    Array = []
+    with open(Filename, 'r') as f:
+        reader = csv.reader(f)
+        if Int:
+            for i in reader:
+                try:
+                    Int_i = []
+                    for j in i:
+                        Int_i += [ float(j)]
+                    Array += [ Int_i ]
+                except:
+                    pass
+        else:
+            for i in reader:
+                Array += [ i ]
+        
+    return(Array[1:])
+
+def groupContexts( Dictionary, Alphabet ):
+    GroupDict = dict()
+    for context in Dictionary:
+        value = Dictionary[context]
+        ConLen = len(context)
+        GroupContext = list( context[: int( (ConLen-1)/2)] ) + [ '*' ] + list( context[ int( (ConLen+1)/2) :] )
+        GroupDict[ tuple( GroupContext ) ] = GroupDict.get( tuple( GroupContext ), [] ) + [value]
+    return(GroupDict)
+
+def ChangeDir():
+    os.chdir("D:\Eclipse\Tse-Dude")
+    
+def AnalyzeContextGroupInfo( Dict ):
+        Ratios = []
+        for i in Dict:
+            Dict[i] = [x / min(Dict[i]) for x in Dict[i]]
+            Ratios += [ numpy.std( Dict[i]) ]
+        Ratios.sort()
+        Ratios = Ratios[::-1]
+        print( "FEW STATS")
+        aa = min(range(len(Ratios)), key=lambda i: abs(Ratios[i]-2))
+        print(  "Good std til 2", aa,"Length",len(Ratios))
+    
+        aa = min(range(len(Ratios)), key=lambda i: abs(Ratios[i]-5))
+#        print( Ratios[ 0: aa ])
+        print(  "Good std till 5", aa,"Length",len(Ratios))  
+        
+def MaximumApperences( Array  ):      
+    InputSequence = Array[0].InputSequence.Sequence
+    SequenceLength = len( InputSequence )
+    FinalOutputSequence = [ None ]*SequenceLength
+    ArrayOfOutputSequence = [] 
+    
+    for output in Array:
+        ArrayOfOutputSequence += [ output.Sequence ]
+    
+    ArrayOfOutputSequence = numpy.array(ArrayOfOutputSequence)
+    for index in range( SequenceLength ):
+        Column = list( ArrayOfOutputSequence[:,index ])
+        FinalOutputSequence[index] = max( Column, key = Column.count)
+    
+    return(FinalOutputSequence)
+
+def VariableContext( Array  ):      
+    InputSequence = Array[0].InputSequence.Sequence
+    ReceivedSequence = Array[0].ReceivedSequence
+    SequenceLength = len( InputSequence )
+    FinalOutputSequence = [ None ]*SequenceLength
+    ArrayOfOutputSequence = [] 
+    GoodEdit = 0
+    BadEdit = 0
+    Nochanges = 0
+    for output in Array:
+        ArrayOfOutputSequence += [ output.Sequence ]
+    
+    ArrayOfOutputSequence = numpy.array(ArrayOfOutputSequence)
+    
+    for index in range( SequenceLength ):
+        Column = list( ArrayOfOutputSequence[:,index ])
+        ReceivedSymbol = ReceivedSequence[ index ]
+        
+        # Remove the recieved symbol
+        TruncatedColumn = list( filter( (ReceivedSymbol ).__ne__, Column ))
+        InputSymbol = InputSequence[index]
+        # Return received symbol if that was the only symbol which received
+        if len( TruncatedColumn ) == 0:
+            FinalOutputSequence[index] = ReceivedSymbol      
+        else:
+            Nochanges += 1
+            OutputSymbol = max( Column, key = Column.count )
+            FinalOutputSequence[index] = OutputSymbol
+            
+            # Other context lengths corrects the min context length output
+            if( InputSymbol == OutputSymbol and InputSymbol != Column[0] ):
+#                 print (" Corrected " )
+#                 print ( index, ":", Column, "Input = ",InputSymbol, "Recevied=", ReceivedSymbol, "output =", OutputSymbol )
+                GoodEdit += 1
+            elif( InputSymbol != OutputSymbol and InputSymbol == Column[0] ):
+#                 print ("Wronged" )
+#                 print ( index, ":", Column, "Input = ",InputSymbol, "Recevied=", ReceivedSymbol, "output =", OutputSymbol )
+                BadEdit += 1
+            else:
+                pass
+    print( "Goodedits", GoodEdit, " BadEdits", BadEdit, "Nochanges", Nochanges)
+    a = input("ENTER")
+    return(FinalOutputSequence)
--- GenomeRead.py
+++ GenomeRead.py
@@ -1,5 +1,9 @@
 from Bio import SeqIO
 import sys, numpy
+import CommonFunctions
+from CommonFunctions import WriteArrayinFile
+from operator import itemgetter
+
 class Genome:
     Name = ""
     DNAList= []
@@ -7,9 +11,11 @@ class Genome:
     ReadLength_Considered = 100
     RepeatsHashDictionary = dict()
     Breakpoint = 100
-    
-    def __init__(self, filename):
-        Handle = open(filename)
+    Filename = ""
+    SideLengths = 5
+    def __init__(self, Filename):
+        Handle = open(Filename)
+        self.Filename = Filename
         for seq_record in SeqIO.parse(Handle, "fasta"):
             self.DNAList.append(str(seq_record.seq))
         print( "Length of the DNA is", len(self.DNAList[0]))
@@ -26,6 +32,66 @@ class Genome:
             return ( self.DNA_current[position:] + self.DNA_current[:Extra] )
         else:
             return ( self.DNA_current[position: position + self.ReadLength_Considered])
+    
+    def Test(self, length = 100):
+        self.ReadLength_Considered = length
+        self.RepeatsHashDictionary = dict()
+        self.DNA_current = self.DNAList[0]
+        Reads = []
+        #2872915
+        for position in range(self.SideLengths, len(self.DNA_current) - self.ReadLength_Considered - self.SideLengths ):#- 2772915):
+            if position % 500000 == 0:
+                print("In position", position, "DNA left", len(self.DNA_current) - position )           
+            Reads += [ [ self.DNA_current[position - self.SideLengths:                             position] ,
+                         self.DNA_current[position                   :position + self.ReadLength_Considered] ,
+                         self.DNA_current[position + self.ReadLength_Considered : position + self.ReadLength_Considered + self.SideLengths]
+                         ] ]
+        print("Sorting Read Array")
+        Reads.sort(key=itemgetter(1), reverse=False)
+        CountInfo = []
+        ReadInfo = []
+        
+        print("Counting Repeats")
+        CurrentString = Reads[0][1]
+        Repeat = 0
+        Titles = [ "Gene", "Gene Length", "Read Length", "Read", "Neighbhours", "Repeat"]
+        WriteInfo = [ [Titles ] ]
+        LeftNeighbhors = []
+        RightNeighbors = []
+        for read in Reads[1:]:
+            if read[1] == CurrentString:
+                Repeat += 1
+                LeftNeighbhors += read[0]
+                RightNeighbors += read[2]
+            else:
+                if len( set(LeftNeighbhors) ) != 0 and len( set(RightNeighbors) ) != 0 and Repeat > 0:
+                    CountInfo += [  Repeat ]
+                    ReadInfo += [ CurrentString ]
+                    WriteInfo += [ [ self.Filename, len(self.DNA_current), self.ReadLength_Considered, read, -1, Repeat ] ]
+                if len( set(LeftNeighbhors) ) == 0 and len( set(RightNeighbors) ) == 0 and Repeat > 0:
+                    print( "Saved!" ,Repeat)
+                Repeat = 0
+                CurrentString = read
+        CountInfo.sort(key=None, reverse=True)
+        CommonFunctions.ReWriteArrayinFile(WriteInfo,'RepeatData.csv')
+        Summary =  [ "Gene",        "DNA length",           "Spectrum Length",          "Number of repeat reads", "Uncertainty upperbound"]
+        Summary += [[ self.Filename, len(self.DNA_current), self.ReadLength_Considered, len(CountInfo), 2*sum(CountInfo) ]]
+        CommonFunctions.ReWriteArrayinFile(Summary, "Summary.csv")
+#         print( CountInfo[:100] )
+        print("Length of the DNA is", len(self.DNA_current))
+        print("Number of reads repeating of length", self.ReadLength_Considered," is", len(CountInfo))
+        print("Uncertainity", 2*sum(CountInfo))
+        
+if len( sys.argv )> 1 :
+    RepeatLength = int( float( sys.argv[1] ) )
+else:
+    RepeatLength = int( 50)
+    
+
+Staphylococcus = Genome("StaphylococcusAureus.fasta")
+Staphylococcus.Test(RepeatLength)
+
+# Rhodobacter = Genome("RhodobacterSphaeroides.fasta")
 # 
 #     def RepeatsofgivenLength(self, length = 100):
 #         self.ReadLength_Considered = length
@@ -43,49 +109,4 @@ class Genome:
 #             self.__IncreamentDictElement(ReadString)
 #         
 #         self.AnalyzeRepeatStructure()
-    
-    def Test(self, length = 100):
-        self.ReadLength_Considered = length
-        self.RepeatsHashDictionary = dict()
-        self.DNA_current = self.DNAList[0]
-        Reads = []
-        for position in range(len(self.DNA_current) - self.ReadLength_Considered):
-             if position % 50000 == 0:
-                 print("In position", position, "DNA left", len(self.DNA_current) - position )
-                
-             Reads += [ self.DNA_current[position:position + self.ReadLength_Considered] ]
-        
-        print("Sorting Read Array")
-        Reads.sort(key=None, reverse=False)
-        CountInfo = []
-        ReadInfo = []
-        
-        print("Counting Repeats")
-        CurrentString = Reads[0]
-        Repeat = 0
-        for read in Reads[1:]:
-            if read == CurrentString:
-                Repeat += 1
-            else:
-                if Repeat > 0:
-                    CountInfo += [  Repeat ]
-                    ReadInfo += [ CurrentString ]
-                Repeat = 0
-                CurrentString = read
-        CountInfo.sort(key=None, reverse=True)
-#         print( CountInfo[:100] )
-        print("Length of the DNA is", len(self.DNA_current))
-        print("Number of reads repeating of length", self.ReadLength_Considered," is", len(CountInfo))
-        print("Uncertainity", 2*sum(CountInfo))
-        
-if len( sys.argv )> 1 :
-    RepeatLength = int( float( sys.argv[1] ) )
-else:
-    RepeatLength = int( 500)
-    
-
-Staphylococcus = Genome("StaphylococcusAureus.fasta")
-Staphylococcus.Test(RepeatLength)
-
-# Rhodobacter = Genome("RhodobacterSphaeroides.fasta")
 # Rhodobacter.RepeatsofgivenLength(100)
